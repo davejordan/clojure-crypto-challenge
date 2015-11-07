691b046500585e684a082c049e7524badecd837e
David Jordan <david.jordan78@gmail.com>
2015-11-07 12:12:30 +0000

    Middle of Set 1. Not passing tests for challenge 3

--- src/clojure_crypto_challenge/core.clj
+++ src/clojure_crypto_challenge/core.clj
@@ -1,8 +1,5 @@
 (ns clojure-crypto-challenge.core
-  (:gen-class))
-
-
-
+  (:require [clojure.string :as string]))
 
 (def base64-code
   (let
@@ -10,17 +7,165 @@
        m (fn [[s e]] (vec (map char (range (byte s) (+ (byte e) 1)))))]
     (conj (reduce #(apply conj %1 %2)  (map m character-range)) \+ \/) ))
 
-
 (defn convert-hexlet
   [b]
   (nth base64-code b))
 
+(defn hexlet-3byte-split
+  "Returns a hexlet representation of a 3 Byte array"
+  [[b1 b2 b3]]
+  [(unsigned-bit-shift-right b1 2) ;h1
+   (bit-or (bit-shift-left (bit-and b1 2r00000011) 4)
+           (unsigned-bit-shift-right b2 4)) ;h2
+   (bit-or (bit-shift-left (bit-and b2 2r00001111) 2)
+           (unsigned-bit-shift-right b3 6)) ;h3
+   (bit-and b3 2r00111111)])
 
-(defn encode-3byte-array
-  [b1 b2 b3]
-  [(convert-hexlet (unsigned-bit-shift-right b1 2))
-   (convert-hexlet (bit-or  (bit-shift-left b1 4) (bit-and b2 0b11110000)))
-   (convert-hexlet (bit-or
-                    (bit-shift-left (bit-and  b2 0b00001111) 2)
-                    (unsigned-bit-shift-right b3 6)))
-   (convert-hexlet (bit-and b3 0b00111111))])
+(defn hexlet-padded-3byte-split
+  "Returns a hexlet representation of a byte array.
+  Expects an array of bytes. Length <= 3."
+  [t]
+  (let [[b1 b2 b3] t
+        s (count t)]
+    (cond
+      (= s 1) (take 2 (hexlet-3byte-split (conj (vec t) 0 0)))
+      (= s 2) (take 3 (hexlet-3byte-split (conj (vec t) 0)))
+      (= s 3) (hexlet-3byte-split t))))
+
+(defn base64-encode
+  "Encode a sequence of bytes in base64 (radix)"
+  [xa]
+  (->>
+   (partition-all 3 xa)
+   ( mapcat hexlet-padded-3byte-split)
+   ( map convert-hexlet)
+   vec))
+
+
+(defn decode-character-base16
+  [x]
+  (Integer/parseInt x 16))
+
+(defn decode-base16
+  "Decode a base16 string to numbers"
+  [xs]
+  (->>
+   xs
+   (partition-all 2)
+   (map (fn [y] (apply str y)))
+   (map decode-character-base16)))
+
+(defn encode-base16
+  "Encode an array of bytes to a string of Hex"
+  [xs]
+  (->>
+   xs
+   (map (fn [y] (format "%h" y)))
+   (apply str)))
+
+;; Set 1 Challenge 2 - Fixed XOR
+(defn fixed-XOR
+  "xor each byte in arrays x and y. x and y must be same length"
+  [x y]
+  (map bit-xor x y))
+
+
+;;; Set 1 Challenge 2 - Single-byte XOR cipher
+
+;;;  Letter  Frequency
+(def letter-frequencies-100
+  {:E 12.02, :T 9.10, :A 8.12, :O 7.68, :I 7.31, :N 6.95,
+   :S 6.28, :R 6.02, :H 5.92, :D 4.32, :L 3.98, :U 2.88, :C 2.71,
+   :M 2.61, :F 2.30, :Y 2.11, :W 2.09, :G 2.03, :P 1.82, :B 1.49,
+   :V 1.11, :K 0.69, :X 0.17, :Q 0.11, :J 0.10, :Z 0.07 })
+
+(def letter-frequencies
+  {:E 0.1202, :T 0.910, :A 0.812, :O 0.768, :I 0.731, :N 0.695,
+   :S 0.628, :R 0.602, :H 0.592, :D 0.432, :L 0.398, :U 0.288, :C 0.271,
+   :M 0.261, :F 0.230, :Y 0.211, :W 0.209, :G 0.203, :P 0.182, :B 0.149,
+   :V 0.111, :K 0.069, :X 0.017, :Q 0.011, :J 0.010, :Z 0.007 })
+
+;; letter count
+
+(def test-string "aababa22f452fffaaa000001234f")
+
+(defn count-occurences
+  [m l]
+  (let [h (first l)
+        f (fn[a] (= h a))]
+    (cond
+      (some? h) (count-occurences
+                 (assoc m h (count (filter f l)))
+                 (remove f l))
+      :else m)))
+
+(defn count-occurences-case-insensitive
+  [l]
+  (->>
+   l
+   (map string/upper-case)
+   (count-occurences {})))
+
+(defn relative-differences
+  [m]
+  (let [c (reduce + (vals m))
+        s (seq m)]
+    (reduce #(assoc
+              %1
+              (first %2)
+              (/ (second %2) c)) {} s)))
+
+
+(defn abs [n] (max n (- n)))
+
+(defn is-word-score
+  "difference the scores for each key in w against reference in m.
+  if key not in m, exception. divides difference by total of reference (m),
+  and subtracts from 1. Assumes total for maps is 1. "
+  [m w]
+  (let [total (reduce + (vals m))]
+    (- 1 (/
+          (reduce
+           +
+           (map #(abs (- (second %) (get m (first %)))) w))
+          total))))
+
+(defn convert-char-to-key
+  [c]
+  (keyword (string/upper-case (str c))))
+
+
+;; HACK
+(defn cat
+  "Convert the values of s to keys, and count occurences"
+  [s]
+  (count-occurences {} (map convert-char-to-key s)))
+
+
+(defn dog
+  "c is occurence map, lf is letter frequency reference.
+  Returns a map of only occurrences that are in lf"
+  [c lf]
+  (let [ks (keys lf)
+        mks (keys (apply dissoc c ks))]
+    (apply dissoc c mks)))
+
+(defn fixed-value-XOR
+  []
+  1 )
+
+(defn rat
+  "Create a list of words-scores aginst full range of possible byte values
+  "
+  [code-string letter-distribution encoding-fn]
+  (let [decoded-code-string (decode-base16 code-string)
+        decoded-code-length (count decoded-code-string)
+        test-byte-range (range 0xff)]
+    (for [x test-byte-range
+          :let [
+                c (repeat decoded-code-length x)
+                xd (fixed-XOR decoded-code-string c)
+                xds (encoding-fn xd)
+                s-occurence-map (cat xd)
+                s-filtered-occurence-map (dog s-occurence-map letter-distribution)]]
+      {x (is-word-score letter-distribution s-filtered-occurence-map)})))
--- test/clojure_crypto_challenge/core_test.clj
+++ test/clojure_crypto_challenge/core_test.clj
@@ -1,6 +1,7 @@
 (ns clojure-crypto-challenge.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-crypto-challenge.core :refer :all]))
+  (:require [clojure-crypto-challenge.core :refer :all]
+            [clojure.string :as string]
+            [clojure.test :refer :all]))
 
 
 (deftest test-hex-to-64
@@ -17,4 +18,105 @@
 
 (deftest test-convert-4byte-array
   (testing "testing 4 byte conversion"
-    (is (= 0 (compare (encode-3byte-array [0 0 0]) [\A \A \A \A])))))
+    (are [x y] (= 0 (compare (base64-encode x) y))
+      [0 0 0] [\A \A \A \A]
+      [0 0 1] [\A \A \A \B]
+      [0 0 63] [\A \A \A \/]
+      [0xff 0xff 0xff] [\/ \/ \/ \/]
+      [0xff 0xff 0xff 0xff] [\/ \/ \/ \/ \/ \w]
+      )))
+
+(deftest test-decode-base16
+  (testing "decode hex characters"
+    (are [x y] (= x (decode-base16 y))
+      '(0) "0"
+      '(15) "f"
+      [255 255 255] "ffffff"
+      [0 0 0] "000000"
+      [0 1] "001")))
+
+
+(def hex-source-test-code
+  "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+
+(def base64-dest-test-code
+  "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+
+(deftest test-challenge-1
+  (testing "compare the challenge strings"
+    (is (= base64-dest-test-code
+           (apply str (base64-encode (decode-base16 hex-source-test-code)))))))
+
+;; Set 1 Challenge 2
+
+(deftest test-fixed-XOR
+  (testing "check XOR of byte arrays"
+    (are [x y z] (= x (fixed-XOR y z))
+      [2r00] [2r00] [2r00]
+      [2r01] [2r00] [2r01]
+      [2r00 2r00] [2r00 2r00] [2r00 2r00]
+      [2r01 2r10 2r00] [2r10 2r01 2r11] [2r11 2r11 2r11])))
+
+(def fixed-xor-source-code-1 "1c0111001f010100061a024b53535009181c")
+(def fixed-xor-source-code-2 "686974207468652062756c6c277320657965")
+(def fixed-xor-dest-code "746865206b696420646f6e277420706c6179")
+
+(deftest test-challenge-2
+  (testing "test Set1 challenge 2"
+    (is (= fixed-xor-dest-code
+           (encode-base16 (fixed-XOR
+                           (decode-base16 fixed-xor-source-code-1)
+                           (decode-base16 fixed-xor-source-code-2)))))))
+
+;;; Set 1 Challenge 3 - Single-byte XOR cipher
+
+(def single-byte-xor-cipher-code "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+
+(deftest test-count-occurences
+  (testing "test count-occurence"
+    (are [x y] (= x (count-occurences {} y))
+      {\a 1} "a"
+      {\a 5} "aaaaa"
+      {} ""
+      {\a 3 \b 3} "ababab")))
+
+(deftest test-relative-differences
+  (testing "test relative-differences calculates on map of counts"
+    (are [x y] (= x (relative-differences y))
+      {\a 1} {\a 1}
+      {\a 1/2, \b 1/2} {\a 1, \b 1}
+      {\a 2/3 \b 1/3} {\a 2, \b 1}
+      {\a 1/3 \b 1/3 \c 1/3} {\a 1 \b 1\c 1}
+      {} {}
+      {:a 1} {:a 1}
+      )))
+
+(deftest test-is-word-score
+  (testing "test the scoring of a string as a phrase"
+    (are [x m y] (== x (is-word-score m y))
+      1 {:A 0.5 :B 0.5} {:A 0.5 :B 0.5}
+      1/3 {:A 1 :B 2} {:A 2 :B 1}
+      1 {:A 1} {:A 1}
+      2/3 {:A 1 :B 2} {:A 2}
+      )))
+
+(deftest test-is-word-throws-exception
+  (testing "is-word-score throws exception if key not in map"
+    (is (thrown? NullPointerException (is-word-score {:A 1} {:A 1 :B 1})))))
+
+
+
+
+
+
+(def d-list (rat single-byte-xor-cipher-code letter-frequencies #(map char %)))
+
+(def th (reduce merge {} d-list))
+
+th
+
+(filter #(= (val %) (apply max (vals th))) th)
+
+
+(apply str (map char (fixed-XOR (decode-base16 single-byte-xor-cipher-code)
+                                (repeat (count single-byte-xor-cipher-code) 249))))
